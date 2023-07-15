22ab3b8b14e7167c2d9da895392729ac0f196969
Farber98 <juantuc98@gmail.com>
2023-07-15 15:28:39 -0300

    round state.

--- programs/slamjam-homerun-v1/src/lib.rs
+++ programs/slamjam-homerun-v1/src/lib.rs
@@ -6,3 +6,25 @@ declare_id!("7rxVoKkHEj63EVcKi4gC3utmgs1D4chGP7HzQneMuyKV");
 pub mod slamjam_homerun_v1 {
     use super::*;
 }
+
+#[account]
+pub struct RoundCounter {
+    // To point current round.
+    round: u32, // 4
+}
+
+#[account]
+pub struct Round {
+    // Who claimed the round
+    claimer: Pubkey, // 32
+
+    // Winner of the round
+    winner: Pubkey, // 32
+
+    // Highest score
+    score: u16, // 2
+
+    // Play until deadline. Winner claims until 2 * deadline (grace period).
+    // After grace period, anyone can claim.
+    deadline: u32, // 4
+}
