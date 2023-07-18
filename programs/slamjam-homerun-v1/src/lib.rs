b332903bb54b84f63b65a43854e8cef1ce96e5ea
Farber98 <juantuc98@gmail.com>
2023-07-17 21:07:03 -0300

    initialize function as constructor and some tests for initializing and playing.

--- programs/slamjam-homerun-v1/src/lib.rs
+++ programs/slamjam-homerun-v1/src/lib.rs
@@ -1,19 +1,48 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program;
+use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 
 declare_id!("7rxVoKkHEj63EVcKi4gC3utmgs1D4chGP7HzQneMuyKV");
+const FEE: u64 = 1 * LAMPORTS_PER_SOL;
 
 #[program]
 pub mod slamjam_homerun_v1 {
+
     use super::*;
     
-    pub fn play(ctx: Context<Play>) -> Result<()> {
-        msg!("Play called");
-        
-        // If round was created in this transaction, deadline must be set.
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+         // If round was created in this transaction, deadline must be set.
         let round = &mut ctx.accounts.round;
+        round.initialized = true;
+        Ok(())
+    } 
+
+    pub fn play(ctx: Context<Play>) -> Result<()> {
+        let round = &mut ctx.accounts.round;
+        
+        let timestamp = Clock::get()?.unix_timestamp.checked_add(3600).unwrap();
+
+        // If round has no deadline, deadline must be set.
         if round.deadline == 0 {
-            round.deadline = Clock::get()?.unix_timestamp.checked_add(3600).unwrap();
+            // Sets deadline = current timestap + HOUR
+            round.deadline = timestamp;
         }
+
+        // If timestamp > round.deadline, it's claiming phase.
+        require!(round.deadline >= timestamp, Errors::PlayInClaimingPhase);
+
+        // Creates context for transfer CPI.
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(), 
+            system_program::Transfer {
+                from: ctx.accounts.player.to_account_info(),
+                to: round.to_account_info(),
+            });
+
+        // Transfers SOL from player to pool
+        system_program::transfer(cpi_context, FEE)?;
+        
+        round.pool += FEE;
 
         Ok(())
     }
@@ -23,6 +52,9 @@ pub mod slamjam_homerun_v1 {
 #[account]
 pub struct Round {
     // Winner of the round
+    initialized: bool, // 1
+
+    // Winner of the round
     winner: Pubkey, // 32
 
     // Highest score
@@ -31,16 +63,39 @@ pub struct Round {
     // Play until deadline. Winner claims until 2 * deadline (grace period).
     // After grace period, anyone can claim.
     deadline: i64, // 8
+    
+    // Pool amount
+    pool: u64
+}
+
+// An enum for custom error codes
+#[error_code]
+pub enum Errors {
+    PlayInClaimingPhase,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init_if_needed, 
+        seeds = [b"round"],
+        bump,
+        payer = initializer, 
+        space = 8 + 1 + 32 + 2 + 8 + 8,
+        constraint = !round.initialized
+    )]
+    pub round: Account<'info, Round>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct Play<'info> {
     #[account(
-        init_if_needed, 
+        mut, 
         seeds = [b"round"],
         bump,
-        payer = player, 
-        space = 8 + 32 + 2 + 8
     )]
     pub round: Account<'info, Round>,
     #[account(mut)]
--- tests/slamjam-homerun-v1.ts
+++ tests/slamjam-homerun-v1.ts
@@ -2,8 +2,10 @@ import * as anchor from "@project-serum/anchor"
 import { Program } from "@project-serum/anchor"
 import { assert, expect } from "chai"
 import { SlamjamHomerunV1 } from "../target/types/slamjam_homerun_v1";
+import * as web3 from '@solana/web3.js'
 import { BN } from "bn.js";
 import { setTimeout } from "timers/promises";
+
 
 
 describe("slamjam-homerun-v1", () => {
@@ -18,9 +20,18 @@ describe("slamjam-homerun-v1", () => {
     program.programId
   )
 
+  const player1 = anchor.web3.Keypair.generate();
+
+  provider.connection.requestAirdrop(
+    player1.publicKey,
+    10 * web3.LAMPORTS_PER_SOL // 10 SOL
+  )
+
+  const FEE = 1 * web3.LAMPORTS_PER_SOL;
+
   describe("Test Suite", () => {
 
-    it("Shouldn't fetch Round before calling play", async () => {
+    it("Shouldn't fetch Round before calling Initialize", async () => {
       try {
         await program.account.round.fetch(roundPDA);
       } catch (error) {
@@ -28,33 +39,80 @@ describe("slamjam-homerun-v1", () => {
       }
     })
 
-    it("Should create Round gracefully", async () => {
+    it("Should create Round when calling Initialize", async () => {
       const tx = await program.methods
-        .play()
+        .initialize()
         .accounts({ round: roundPDA })
         .rpc()
 
-      const round = await program.account.round.fetch(roundPDA)
+      const round = await program.account.round.fetch(roundPDA);
 
+      expect(round.initialized).to.be.equal(true)
+      expect(round.winner.toBase58()).to.be.equal(new anchor.web3.PublicKey(0).toBase58())
+      expect(round.score).to.be.equal(0)
+      expect(round.deadline.toNumber()).to.be.equal(0)
+      expect(round.pool.toNumber()).to.be.equal(0)
+    })
+
+    it("Shouldn't be able to call Initialize twice", async () => {
+      try {
+        await program.methods
+          .initialize()
+          .accounts({ round: roundPDA })
+          .rpc()
+      } catch (error) {
+        assert.strictEqual(error.message, 'AnchorError caused by account: round. Error Code: ConstraintRaw. Error Number: 2003. Error Message: A raw constraint was violated.');
+      }
+    })
+
+    it("Should play gracefully", async () => {
+      let round = await program.account.round.fetch(roundPDA);
+      const player1BalanceBefore = await program.provider.connection.getBalance(player1.publicKey);
+      const roundPoolBefore = round.pool
+      const roundBalanceBefore = await program.provider.connection.getBalance(roundPDA);
+
+      // Assert deadline was zero before calling first time.
+      expect(round.deadline.toNumber()).to.be.equal(0)
+
+      const tx = await program.methods
+        .play()
+        .accounts({
+          round: roundPDA,
+          player: player1.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId
+        })
+        .signers([player1])
+        .rpc()
+
+
+      round = await program.account.round.fetch(roundPDA);
+
+      // Assert deadline is set and approx an hour.
       const deadlineToDate = new Date(round.deadline.toNumber() * 1000)
       const currentTimestamp = new Date()
-      const HOUR = 59 * 60 * 1000
-      const currentTimestampPlusHour = new Date(currentTimestamp.setTime(currentTimestamp.getTime() + HOUR))
+      const HOURLower = 59 * 60 * 1000
+      const HOURUpper = 61 * 60 * 1000
+      const currentTimestampLower = new Date(currentTimestamp.setTime(currentTimestamp.getTime() + HOURLower))
+      const currentTimestampUpper = new Date(currentTimestamp.setTime(currentTimestamp.getTime() + HOURUpper))
+      expect(deadlineToDate).to.be.gte(currentTimestampLower)
+      expect(deadlineToDate).to.be.lte(currentTimestampUpper)
 
-      expect(deadlineToDate).to.be.gte(currentTimestampPlusHour)
-      expect(round.score).to.be.equal(0)
-      expect(round.winner.toBase58()).to.be.equal(new anchor.web3.PublicKey(0).toBase58())
+      // Assert player balance gets subtracted.
+      const player1BalanceAfter = await program.provider.connection.getBalance(player1.publicKey);
+      expect(player1BalanceAfter).to.be.equal(player1BalanceBefore - FEE)
+
+      // Assert round balance is added
+      const roundBalanceAfter = await program.provider.connection.getBalance(roundPDA);
+      expect(roundBalanceAfter).to.be.equal(roundBalanceBefore + FEE)
+
+      // Assert pool is added
+      const FeeToBN = new BN(FEE)
+      const roundPoolAfter = round.pool
+      expect(roundPoolAfter.toString()).to.be.equal(roundPoolBefore.add(FeeToBN).toString())
     })
 
-    it("Should be able to fetch Round after it is created", async () => {
-      await program.account.round.fetch(roundPDA);
-    })
-
-    it("Should transfer fee from sender to pool", async () => {
-    })
-
-    it("Shouldn't be able to play when deadline is exceeded", async () => {
-      await program.account.round.fetch(roundPDA);
+    it("Shouldn't be able to play after deadline", async () => {
+      expect(true).to.be.equal(false);
     })
 
   })
